dbbab1086243da224fa7a2881cbed8caae4cb8aa
PatrickTDewey <ptddev@protonmail.com>
2021-08-28 10:49:40 -0700

    added arrow functions

--- fundamentals/hoisting.js
+++ fundamentals/hoisting.js
@@ -109,20 +109,28 @@
 // burbank
 // sanjose
 
-function makeDojo(name, students){
-    const dojo = {};
-    dojo.name = name;
-    dojo.students = students;
-    if(dojo.students > 50){
-        dojo.hiring = true;
-    }
-    else if(dojo.students <= 0){
-        dojo = "closed for now";
-    }
-    return dojo;
-}
-console.log(makeDojo("Chicago", 65));
-console.log(makeDojo("Berkeley", 0));
+// function makeDojo(name, students){
+//     const dojo = {};
+//     dojo.name = name;
+//     dojo.students = students;
+//     if(dojo.students > 50){
+//         dojo.hiring = true;
+//     }
+//     else if(dojo.students <= 0){
+//         dojo = "closed for now";
+//     }
+//     return dojo;
+// }
+// console.log(makeDojo("Chicago", 65));
+// console.log(makeDojo("Berkeley", 0));
 
 // Output
 // cant reassign dojo
+
+console.log(add(2,3));
+function add(a, b) {
+    return a + b;
+}
+const add; // undefined
+add(2,3)
+add = (a, b) => a + b;
