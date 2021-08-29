b4fb9b03e9ed373846c4eaa6e251bb033dd84396
PatrickTDewey <ptddev@protonmail.com>
2021-08-28 22:55:17 -0700

    added super ninja

--- oop/ninja.js
+++ oop/ninja.js
@@ -17,8 +17,26 @@ class Ninja {
         this.health += 10;
     }
 }
+
+class Sensei extends Ninja{
+    constructor(name, wisdom = 10){
+        super(name, 10, 10)
+        this.wisdom = wisdom;
+        this.health = 200;
+    }
+    speakWisdom(){
+        this.drinkSake();
+        console.log(
+            `If you give me 5 hours to chop down a cherry tree, I will spend four of those hours sharpening my axe.`
+        );
+        
+    }
+    
+}
 const ninja1 = new Ninja("Golden Boy");
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
+const sensei1 = new Sensei('Splinter')
+console.log(sensei1.speakWisdom(), sensei1.showStats());
