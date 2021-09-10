91dbe7c31475259ef9ecce67f7cd967e49862953
PatrickTDewey <ptddev@protonmail.com>
2021-09-10 12:24:04 -0700

    removed unecessary useHistory callback funcs

--- full_stack/product_manager/client/src/views/Detail.js
+++ full_stack/product_manager/client/src/views/Detail.js
@@ -1,23 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import { useParams, useHistory, Link } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 const Detail = () => {
     const [product, setProduct] = useState()
     const { id } = useParams()
-    const history = useHistory()
     useEffect(() => {
         axios.get("http://localhost:8000/api/products/view/" + id)
             .then(res => setProduct(res.data))
     }, [id])
-    const clickHandler = (e) => {
-        e.preventDefault()
-        history.goBack()
-    }
-    const updateClick = (e) => {
-        e.preventDefault()
-        history.push(`/products/update/${id}`)
-        history.goForward()
-    }
     return (
         <div>
             {
